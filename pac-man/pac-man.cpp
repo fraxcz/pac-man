a63fc8e73c2c00b114740f896c72b36349261b56
fraxcz <124736819+fraxcz@users.noreply.github.com>
2024-11-29 17:04:24 +0100

    Přidejte soubory projektu.

---
+++ pac-man/pac-man.cpp
@@ -0,0 +1,9 @@
+﻿// pac-man.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
